b8029bbb9c2ad38c802faa82bfd6e0213c612d6f
fairuzn <60942128+fairuzn@users.noreply.github.com>
2023-02-11 23:34:36 +0000

    Update for Fairuz

--- particles/fairuz_particle.py
+++ particles/fairuz_particle.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import numpy as np
+import math
 
 # import the child classes
 #from particles.particle_handler import ParticleHandler
@@ -42,8 +43,8 @@ class FairuzParticle: #(ParticleHandler):
 
 #
 	
-	def createParticlesDict(self, color, x, y, radius):
-		return {"color": color, "x": x, "y": y, "radius": radius}
+	def createParticlesDict(self, color, x, y, radius, vx, vy):
+		return {"color": color, "x": x, "y": y, "radius": radius, "vx": vx, "vy": vy}
 	
 	# updates particle dictionary above and returns a list of multiple particles
 	def multipleParticles(self, numOfParticles, color, radius):
@@ -52,13 +53,15 @@ class FairuzParticle: #(ParticleHandler):
 			#color = random.choice(self.colors_list)
 			#x = random.randint(0, 1600)
 			#y = random.randint(0, 900)
+			vx = 0
+			vy = 0
 			if color == self.red:
 				x = random.randint(100, 600)
 				y = random.randint(50, 400)
 			elif color == self.cyan:
 				x = random.randint(800, 1500)
 				y = random.randint(400, 800)
-			particle_list.append(self.createParticlesDict(color, x, y, radius))
+			particle_list.append(self.createParticlesDict(color, x, y, radius, vx, vy))
 		return particle_list
 
 	
@@ -70,6 +73,42 @@ class FairuzParticle: #(ParticleHandler):
 			self.drawParticle(particle["color"], particle["x"], particle["y"], particle["radius"])
 
 #	
+	def interaction(self, particles1, particles2, particles3, G):
+		for i in range(len(particles1)):
+			fx = 0
+			fy = 0
+
+			for j in range(len(particles2)):
+				pA = particles1[i]
+				pB = particles2[j]
+				dx = pA["x"] - pB["x"]
+				dy = pA["y"] - pB["y"]
+				r = math.sqrt(dx**2 + dy**2)
+
+				if r > 0:
+					F = G * 1/r
+					fx += F*dx
+					fy += F*dy
+			
+			pA["vx"] = (pA["vx"] + fx)/2
+			pB["vy"] = (pA["vy"] + fy)/2
+
+			pA["x"] += pA["vx"]
+			pA["y"] += pA["vy"]
+
+		for k in range(len(particles3)):
+			pC = particles3[k]
+			pC["x"] += pA["vx"]*2
+			pC["y"] += pC["vy"]*2
+
+			
+		self.window.fill(0)
+
+			
+
+
+		
+
 	def update(self):
 		""" Called at the start of each frame. Update x and y here """
 		"""
@@ -91,6 +130,8 @@ class FairuzParticle: #(ParticleHandler):
 			self.createParticles(self.cyanParticles)
 
 			pygame.display.update()
+
+			self.interaction(self.cyanParticles, self.cyanParticles, self.redParticles, -0.5)
 
 		pygame.quit()
 
